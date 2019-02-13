9d3ff5dd4d4e03d03d940c09d84e452abe25916d
Chaz <ctol1994@gmail.com>
2019-02-13 13:42:00 -0800

    adjusted timer to go off when event starts

---
+++ client/src/components/Timer.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+export default class Timer extends Component {
+
+  constructor(props) {
+    super(props);
+    this.tick = this.tick.bind(this);
+    this.getNewTimerState = this.getNewTimerState.bind(this);
+    this.state = this.getNewTimerState();
+  }
+
+  getNewTimerState() {
+    let diffMs = (new Date('March 3, 2019 09:00:00 PST') - new Date()); // milliseconds between now & Christmas
+
+    let diffDays = Math.floor(diffMs / 86400000); // days
+    let diffHrs = Math.floor(((diffMs % 86400000) / 3600000) + (diffDays*24)); // hours
+    let diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000); // minutes
+    let diffSecs = Math.round(((diffMs % 86400000) % 3600000) % 60000 / 1000); // seconds
+    return {
+      seconds: diffSecs, // responsible for the seconds
+      minutes: diffMins, // responsible for the minutes
+      hours: diffHrs, // responsible for the hours      
+      // days: diffDays, // responsible for the days      
+    };
+  }
+
+  tick() {
+    this.setState(this.getNewTimerState());
+  }
+
+  componentDidMount() {
+    // update every second
+    this.interval = setInterval(this.tick, 1000);
+  }
+
+  render() {    
+
+    // If it is within 36 hours of the hackathon show the countdown. To enable the 36 hour check, replace 'true' with 'showTimer'
+    const startTimer = (new Date() < new Date('March 1, 2019 21:00:00 PST')) ? false : true;
+    if(startTimer)
+      return (
+        <div className="Timer">
+          <div><span>{this.state.hours}</span><br/>HRS</div>
+          <div><span>{this.state.minutes}</span><br/>MINS</div>
+          <div><span>{this.state.seconds}</span><br/>SECS</div>
+        </div>
+      );
+    return (
+      <div className="Timer">
+        <div><span>{'36'}</span><br/>HRS</div>
+        <div><span>{'00'}</span><br/>MINS</div>
+        <div><span>{'00'}</span><br/>SECS</div>
+      </div>
+    );
+  }
+}
