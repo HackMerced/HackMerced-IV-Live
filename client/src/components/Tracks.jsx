a1903a40085ccf937e66b9377c9d099c9ca80974
Chris DeSoto <cdesoto22@yahoo.com>
2019-02-25 12:56:12 -0800

    Added some icons for each of the main tracks

--- client/src/components/Tracks.jsx
+++ client/src/components/Tracks.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Icon } from 'react-icons-kit'
+import { globe, spaceShuttle, tree } from 'react-icons-kit/fa/'
+import '../assets/css/Tracks.css';
 
-const Tracks = () => (
-    <div>{'Tracks'}</div>
-);
+export default class Tracks extends React.Component {
+    constructor(props) {
+        super(props); 
+        this.state = {
+            iconSize: 128
+        };
+    }
 
-export default Tracks;
+    render() {
+        return ( 
+            <div className="Tracks">
+                <div><Icon size={this.state.iconSize} icon={globe} /><br/><p>Social Good</p></div>
+                <div><Icon size={this.state.iconSize} icon={spaceShuttle} /><br/><p>Entrepreneurship</p></div>
+                <div><Icon size={this.state.iconSize} icon={tree} /><br/><p>Agtech</p></div>
+            </div>
+        );
+    }
+
+}
