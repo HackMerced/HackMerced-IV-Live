c605823685931cdcb6931c79548c4e66414826d1
Chris DeSoto <cdesoto22@yahoo.com>
2019-02-28 12:10:08 -0800

    Added tracks and subcategories

--- client/src/components/Tracks.jsx
+++ client/src/components/Tracks.jsx
@@ -1,30 +1,91 @@
 import React from 'react';
 import { Icon } from 'react-icons-kit'
-import { globe, spaceShuttle, tree } from 'react-icons-kit/fa/'
+import { globe, spaceShuttle, tree, shareAlt, gamepad, unlock, trophy } from 'react-icons-kit/fa/'
+import { ic_developer_mode, ic_devices } from 'react-icons-kit/md/'
 import '../assets/css/Tracks.css';
 
 export default class Tracks extends React.Component {
     constructor(props) {
         super(props); 
         this.state = {
-            iconSize: 50
+            tracksIconSize: 50,
+            subCatsIconSize: 34,
+            trophyIconSize: 20
         };
     }
 
     render() {
-        return ( 
-            <div className="Tracks">
-                <div>
-                    <Icon className="TrackIcon" size={this.state.iconSize} icon={globe} /><span>Social Good</span>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis venenatis ante, vel pellentesque sapien aliquet id. Praesent porta et purus eget imperdiet. Nulla augue lorem, sagittis id lorem bibendum, elementum sagittis velit. Duis et porttitor eros. Aenean ipsum nibh, malesuada bibendum congue consectetur, commodo id magna. Cras tincidunt scelerisque massa id luctus. Cras suscipit in diam non congue. Nam ut urna at nisl efficitur varius sit amet sed purus. Nullam vel orci turpis. Praesent mattis, arcu sed condimentum tempus, nisl tortor pretium ligula, dignissim ultrices lorem eros vel ante. Ut in tempor est.</p>
+        return (
+            <div className="TracksContainer"> 
+                <h3><i>Main Tracks</i></h3>
+                <div className="Tracks">
+                    <div>
+                        <Icon className="TrackIcon" size={this.state.tracksIconSize} icon={globe} /><span>Social Good</span>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis venenatis ante, vel pellentesque sapien aliquet id. Praesent porta et purus eget imperdiet. Nulla augue lorem, sagittis id lorem bibendum, elementum sagittis velit. Duis et porttitor eros. Aenean ipsum nibh, malesuada bibendum congue consectetur, commodo id magna.</p>
+                        <div className="Prizes">
+                            <Icon className="TrophyIcon" size={this.state.trophyIconSize} icon={trophy} />
+                            <span>Charles will do your dishes.</span>
+                        </div>
+                    </div>
+                    <div>
+                        <Icon className="TrackIcon" size={this.state.tracksIconSize} icon={spaceShuttle} /><span>Entrepreneurship</span>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis venenatis ante, vel pellentesque sapien aliquet id. Praesent porta et purus eget imperdiet. Nulla augue lorem, sagittis id lorem bibendum, elementum sagittis velit. Duis et porttitor eros. Aenean ipsum nibh, malesuada bibendum congue consectetur, commodo id magna.</p>
+                        <div className="Prizes">
+                            <Icon className="TrophyIcon" size={this.state.trophyIconSize} icon={trophy} />
+                            <span>Adrian will clean your room.</span>
+                        </div>
+                    </div>
+                    <div>
+                        <Icon className="TrackIcon" size={this.state.tracksIconSize} icon={tree} /><span>Agtech</span>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis venenatis ante, vel pellentesque sapien aliquet id. Praesent porta et purus eget imperdiet. Nulla augue lorem, sagittis id lorem bibendum, elementum sagittis velit. Duis et porttitor eros. Aenean ipsum nibh, malesuada bibendum congue consectetur, commodo id magna.</p>
+                        <div className="Prizes">
+                            <Icon className="TrophyIcon" size={this.state.trophyIconSize} icon={trophy} />
+                            <span>Charles will do your laundry.</span>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    <Icon className="TrackIcon" size={this.state.iconSize} icon={spaceShuttle} /><span>Entrepreneurship</span>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis venenatis ante, vel pellentesque sapien aliquet id. Praesent porta et purus eget imperdiet. Nulla augue lorem, sagittis id lorem bibendum, elementum sagittis velit. Duis et porttitor eros. Aenean ipsum nibh, malesuada bibendum congue consectetur, commodo id magna. Cras tincidunt scelerisque massa id luctus. Cras suscipit in diam non congue. Nam ut urna at nisl efficitur varius sit amet sed purus. Nullam vel orci turpis. Praesent mattis, arcu sed condimentum tempus, nisl tortor pretium ligula, dignissim ultrices lorem eros vel ante. Ut in tempor est.</p>
-                </div>
-                <div>
-                    <Icon className="TrackIcon" size={this.state.iconSize} icon={tree} /><span>Agtech</span>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis venenatis ante, vel pellentesque sapien aliquet id. Praesent porta et purus eget imperdiet. Nulla augue lorem, sagittis id lorem bibendum, elementum sagittis velit. Duis et porttitor eros. Aenean ipsum nibh, malesuada bibendum congue consectetur, commodo id magna. Cras tincidunt scelerisque massa id luctus. Cras suscipit in diam non congue. Nam ut urna at nisl efficitur varius sit amet sed purus. Nullam vel orci turpis. Praesent mattis, arcu sed condimentum tempus, nisl tortor pretium ligula, dignissim ultrices lorem eros vel ante. Ut in tempor est.</p>
+                <h3><i>Subcategories</i></h3>
+                <div className="Subcategories">
+                    <div>
+                        <Icon className="TrackIcon" size={this.state.subCatsIconSize} icon={ic_developer_mode} /><br/><span>CSxA Award</span>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis venenatis ante, vel pellentesque sapien aliquet id. Praesent porta et purus eget imperdiet. Nulla augue lorem, sagittis id lorem bibendum, elementum sagittis velit.</p>
+                        <div className="Prizes">
+                            <Icon className="TrophyIcon" size={this.state.trophyIconSize} icon={trophy} />
+                            <span>$250 to your team or a charity of your choice</span>
+                        </div>
+                    </div>
+                    <div>
+                        <Icon className="TrackIcon" size={this.state.subCatsIconSize} icon={shareAlt} /><br/><span>Best Social Media</span>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis venenatis ante, vel pellentesque sapien aliquet id. Praesent porta et purus eget imperdiet. Nulla augue lorem, sagittis id lorem bibendum, elementum sagittis velit.</p>
+                        <div className="Prizes">
+                            <Icon className="TrophyIcon" size={this.state.trophyIconSize} icon={trophy} />
+                            <span>2<sup>nd</sup> Genereation Amazon Echo Dot (4)</span>
+                        </div>
+                    </div>
+                    <div>
+                        <Icon className="TrackIcon" size={this.state.subCatsIconSize} icon={unlock} /><br/><span>Best Beginner Hack</span>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis venenatis ante, vel pellentesque sapien aliquet id. Praesent porta et purus eget imperdiet. Nulla augue lorem, sagittis id lorem bibendum, elementum sagittis velit.</p>
+                        <div className="Prizes">
+                            <Icon className="TrophyIcon" size={this.state.trophyIconSize} icon={trophy} />
+                            <span>makey makey classic kits</span>
+                        </div>
+                    </div>                
+                    <div>
+                        <Icon className="TrackIcon" size={this.state.subCatsIconSize} icon={gamepad} /><br/><span>Best Game (2)</span>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis venenatis ante, vel pellentesque sapien aliquet id. Praesent porta et purus eget imperdiet. Nulla augue lorem, sagittis id lorem bibendum, elementum sagittis velit.</p>
+                        <div className="Prizes">
+                            <Icon className="TrophyIcon" size={this.state.trophyIconSize} icon={trophy} />
+                            <span>Coolermaster keyboard, earbuds, and mouse</span>
+                        </div>
+                    </div>                
+                    <div>
+                        <Icon className="TrackIcon" size={this.state.subCatsIconSize} icon={ic_devices} /><br/><span>Wolfram Alpha API</span>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis venenatis ante, vel pellentesque sapien aliquet id. Praesent porta et purus eget imperdiet. Nulla augue lorem, sagittis id lorem bibendum, elementum sagittis velit.</p>
+                        <div className="Prizes">
+                            <Icon className="TrophyIcon" size={this.state.trophyIconSize} icon={trophy} />
+                            <span>???</span>
+                        </div>
+                    </div>                
                 </div>
             </div>
         );
