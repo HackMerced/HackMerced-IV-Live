39dc0af167befb7f452cf3a612d803ff3d5a32e2
Adrian Darian <adarian@ucmerced.edu>
2019-03-02 13:46:35 -0800

    submission

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -16,9 +16,9 @@ class NavBar extends Component {
                     <Link className = "menu-item" to = {"/"}>Schedule</Link>
                     {/* <Link className = "menu-item" to = "/Map">Map</Link> */}
                     <Link className = "menu-item" to = {"/Prizes"}>Prizes</Link>
-                    {/*<Link className = "menu-item" to = "/Hardware">Hardware</Link>
-                    <Link className = "menu-item" to = "/Submission">Submission</Link>*/}
+                    {/*<Link className = "menu-item" to = "/Hardware">Hardware</Link>*/}
                     <a href='https://join.slack.com/t/hackmerced2019/shared_invite/enQtNTY1NjQ1NjU5OTczLWFlOTJlNDYzZDQyYTdkMmFkZmUzOTgwNDAwNjUxY2RmNzlhOGYwM2Q2NDc3ZmEwMzhjNmNiZWMzNzNjMGYxYTE' target="_blank" rel="noreferrer noopener">Slack</a>
+                    <a href='https://hackmerced-2019.devpost.com/' target="_blank" rel="noreferrer noopener">Submission</a>
                 </Menu>              
             </React.Fragment>
           );
